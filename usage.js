15288b6d462c76fd909643c49762132bf50b009d
homesin <58244938+homesin@users.noreply.github.com>
2020-01-19 06:28:11 +0800

    20200119
    20200119

--- usage.js
+++ usage.js
@@ -2,7 +2,7 @@ var time=new Date();
 const year=time.getFullYear()-1911;
 const lastyear=year-1;
 const month=time.getMonth();
-document.getElementById('btn1').innerText =year+ '年用電情形';
+//document.getElementById('btn1').innerText =year+ '年用電情形';
 var results;
 Papa.parse('usage.txt', {
     header: true,
@@ -15,6 +15,26 @@ Papa.parse('usage.txt', {
     }
 })
 
+Papa.parse('water.txt', {
+    header: true,
+    download: true,
+    dynamicTyping: true,
+    skipEmptyLines: true,
+    complete: function(r) {
+       wresults=r.data; 
+       //與去年相比();      
+    }
+})
+
+Papa.parse('tpwr.txt', {
+    header: true,
+    download: true,
+    dynamicTyping: true,
+    skipEmptyLines: true,
+    complete: function(r) {
+       tresults=r.data;           
+    }
+})
 
 function 所有年度(){
 
@@ -103,11 +123,18 @@ function 自行定義(){
     //sum = $.pivotUtilities.subtotal_aggregators["Sum As Fraction Of Parent Row"];
     $("#output")
         .pivotUI(result, {
-            //dataClass: $.pivotUtilities.SubtotalPivotData,            
+            dataClass: $.pivotUtilities.SubtotalPivotData,            
             rows: ["校區","用電建築物","公開報表序號","帳單年"],
             cols: ["帳單月"] ,
             //aggregator: sum(["總用電度"]),
-           //renderers: $.pivotUtilities.subtotal_renderers,
+            //renderer:$.pivotUtilities.subtotal_renderers["Table With Subtotal Row Heatmap"],
+            //renderers: $.pivotUtilities.subtotal_renderers,
+            renderers:$.extend(
+                $.pivotUtilities.subtotal_renderers,
+                $.pivotUtilities.plotly_renderers,
+                $.pivotUtilities.d3_renderers,
+                $.pivotUtilities.export_renderers
+                ),
             //rendererName: "Table With Subtotal"                    
         })      
 }
@@ -269,6 +296,284 @@ function 今年(){
                                     .range(["#77F", "#FFF", "#F77"])
                             }
                         }*/
+            }
+    });   
+}
+
+function 所有年度用水(){
+
+    result=JSON.parse(JSON.stringify(wresults));   
+    result.forEach(function (element, index){
+        element["水費月"]= element["水費月"]+"月";
+        element["水費年"]=element["水費年"]+"年";          
+    })  
+    $("#output")
+        .pivot(result, {
+            dataClass: $.pivotUtilities.SubtotalPivotData,                    
+            rows: ["校區","供水區","水費年"],
+            cols: ["水費月"] ,
+            aggregator: $.pivotUtilities.aggregators["Integer Sum"](["用水度數"]),
+            sorters: {
+                "校區": $.pivotUtilities.sortAs(["蘭潭","民雄","新民","林森"]),
+            },
+            renderer: $.pivotUtilities.subtotal_renderers["Table With Subtotal Row Heatmap"],                    
+            renderers:$.extend(
+                $.pivotUtilities.subtotal_renderers,
+                $.pivotUtilities.plotly_renderers,
+                $.pivotUtilities.d3_renderers,
+                $.pivotUtilities.export_renderers
+                ),
+            rendererOptions: {                        
+                rowSubtotalDisplay: {
+                disableAfter: 1
+                },                        
+                colSubtotalDisplay: {
+                    disableAfter: 0
+                },
+            }
+    });   
+}
+
+function 今年用水(){
+
+    result=JSON.parse(JSON.stringify(wresults));  
+    result=result.filter(word=>word['水費年']==year); 
+    result.forEach(function (element, index){
+        element["水費月"]= element["水費月"]+"月";
+        element["水費年"]=element["水費年"]+"年";          
+    })   
+    
+    $("#output")
+        .pivot(result, {
+            dataClass: $.pivotUtilities.SubtotalPivotData,                    
+            rows: ["校區","供水區"],
+            cols: ["水費月"] ,
+            aggregator: $.pivotUtilities.aggregators["Integer Sum"](["用水度數"]),
+            sorters: {
+                "校區": $.pivotUtilities.sortAs(["蘭潭","民雄","新民","林森"]),
+            },
+            renderer: $.pivotUtilities.subtotal_renderers["Table With Subtotal Row Heatmap"],                    
+            renderers:$.extend(
+                $.pivotUtilities.subtotal_renderers,
+                $.pivotUtilities.plotly_renderers,
+                $.pivotUtilities.d3_renderers,
+                $.pivotUtilities.export_renderers
+                ),
+            rendererOptions: {                        
+                rowSubtotalDisplay: {
+                disableAfter: 1
+                },                        
+                colSubtotalDisplay: {
+                    disableAfter: 0
+                },
+            }
+    });   
+}
+
+function 用水與去年相比(){
+
+    result=JSON.parse(JSON.stringify(wresults));   
+    result=result.filter(word=>word['水費年']==lastyear | word['水費年']==year); 
+    result.forEach(function (element, index){
+        element["水費月"]= element["水費月"]+"月";
+        element["水費年"]=element["水費年"]+"年";          
+    })        
+    $("#output")
+        .pivot(result, {
+            dataClass: $.pivotUtilities.SubtotalPivotData,                    
+            rows: ["校區","供水區","水費年"],
+            cols: ["水費月"] ,
+            aggregator: $.pivotUtilities.aggregators["Integer Sum"](["用水度數"]),
+            sorters: {
+                "校區": $.pivotUtilities.sortAs(["蘭潭","民雄","新民","林森"]),
+            },
+            renderer: $.pivotUtilities.subtotal_renderers["Table With Subtotal Row Heatmap"],                    
+            renderers:$.extend(
+                $.pivotUtilities.subtotal_renderers,
+                $.pivotUtilities.plotly_renderers,
+                $.pivotUtilities.d3_renderers,
+                $.pivotUtilities.export_renderers
+                ),
+            rendererOptions: {                        
+                rowSubtotalDisplay: {
+                disableAfter: 1
+                },                        
+                colSubtotalDisplay: {
+                    disableAfter: 0
+                },
+            }
+    });   
+}
+
+function 用水自行定義(){
+
+    result=JSON.parse(JSON.stringify(wresults));   
+    result.forEach(function (element, index){
+        element["水費月"]= element["水費月"]+"月";
+        element["水費年"]=element["水費年"]+"年";          
+    })  
+    $("#output")
+        .pivotUI(result, {
+            dataClass: $.pivotUtilities.SubtotalPivotData,                    
+            rows: ["校區","供水區","水費年"],
+            cols: ["水費月"] ,
+            aggregator: $.pivotUtilities.aggregators["Integer Sum"](["用水度數"]),
+            sorters: {
+                "校區": $.pivotUtilities.sortAs(["蘭潭","民雄","新民","林森"]),
+            },
+            renderer: $.pivotUtilities.subtotal_renderers["Table With Subtotal Row Heatmap"],                    
+            renderers:$.extend(
+                $.pivotUtilities.subtotal_renderers,
+                $.pivotUtilities.plotly_renderers,
+                $.pivotUtilities.d3_renderers,
+                $.pivotUtilities.export_renderers
+                ),
+            rendererOptions: {                        
+                rowSubtotalDisplay: {
+                disableAfter: 1
+                },                        
+                colSubtotalDisplay: {
+                    disableAfter: 0
+                },
+            }
+    });   
+}
+
+function 所有年度用電(){
+
+    result=JSON.parse(JSON.stringify(tresults));   
+    result.forEach(function (element, index){
+        element["收費月"]= element["收費月"]+"月";
+        element["收費年"]=element["收費年"]+"年";          
+    })  
+    $("#output")
+        .pivot(result, {
+            dataClass: $.pivotUtilities.SubtotalPivotData,                    
+            rows: ["校區","校區電號別","收費年"],
+            cols: ["收費月"] ,
+            aggregator: $.pivotUtilities.aggregators["Integer Sum"](["用電數"]),
+            sorters: {
+                "校區": $.pivotUtilities.sortAs(["蘭潭","民雄","新民","林森"]),
+            },
+            renderer: $.pivotUtilities.subtotal_renderers["Table With Subtotal Row Heatmap"],                    
+            renderers:$.extend(
+                $.pivotUtilities.subtotal_renderers,
+                $.pivotUtilities.plotly_renderers,
+                $.pivotUtilities.d3_renderers,
+                $.pivotUtilities.export_renderers
+                ),
+            rendererOptions: {                        
+                rowSubtotalDisplay: {
+                disableAfter: 1
+                },                        
+                colSubtotalDisplay: {
+                    disableAfter: 0
+                },
+            }
+    });   
+}
+
+function 今年用電(){
+
+    result=JSON.parse(JSON.stringify(tresults));  
+    result=result.filter(word=>word['收費年']==year); 
+    result.forEach(function (element, index){
+        element["收費月"]= element["收費月"]+"月";
+        element["收費年"]=element["收費年"]+"年";          
+    })    
+    
+    $("#output")
+        .pivot(result, {
+            dataClass: $.pivotUtilities.SubtotalPivotData,                    
+            rows: ["校區","校區電號別"],
+            cols: ["收費月"] ,
+            aggregator: $.pivotUtilities.aggregators["Integer Sum"](["用電數"]),
+            sorters: {
+                "校區": $.pivotUtilities.sortAs(["蘭潭","民雄","新民","林森"]),
+            },
+            renderer: $.pivotUtilities.subtotal_renderers["Table With Subtotal Row Heatmap"],                    
+            renderers:$.extend(
+                $.pivotUtilities.subtotal_renderers,
+                $.pivotUtilities.plotly_renderers,
+                $.pivotUtilities.d3_renderers,
+                $.pivotUtilities.export_renderers
+                ),
+            rendererOptions: {                        
+                rowSubtotalDisplay: {
+                disableAfter: 1
+                },                        
+                colSubtotalDisplay: {
+                    disableAfter: 0
+                },
+            }
+    });   
+}
+
+function 用電與去年相比(){
+
+    result=JSON.parse(JSON.stringify(tresults));   
+    result=result.filter(word=>word['收費年']==lastyear | word['收費年']==year); 
+    result.forEach(function (element, index){
+        element["收費月"]= element["收費月"]+"月";
+        element["收費年"]=element["收費年"]+"年";          
+    })         
+    $("#output")
+        .pivot(result, {
+            dataClass: $.pivotUtilities.SubtotalPivotData,                    
+            rows: ["校區","校區電號別","收費年"],
+            cols: ["收費月"] ,
+            aggregator: $.pivotUtilities.aggregators["Integer Sum"](["用電數"]),
+            sorters: {
+                "校區": $.pivotUtilities.sortAs(["蘭潭","民雄","新民","林森"]),
+            },
+            renderer: $.pivotUtilities.subtotal_renderers["Table With Subtotal Row Heatmap"],                    
+            renderers:$.extend(
+                $.pivotUtilities.subtotal_renderers,
+                $.pivotUtilities.plotly_renderers,
+                $.pivotUtilities.d3_renderers,
+                $.pivotUtilities.export_renderers
+                ),
+            rendererOptions: {                        
+                rowSubtotalDisplay: {
+                disableAfter: 1
+                },                        
+                colSubtotalDisplay: {
+                    disableAfter: 0
+                },
+            }
+    });   
+}
+
+function 用電自行定義(){
+
+    result=JSON.parse(JSON.stringify(tresults));   
+    result.forEach(function (element, index){
+        element["收費月"]= element["收費月"]+"月";
+        element["收費年"]=element["收費年"]+"年";          
+    }) 
+    $("#output")
+        .pivotUI(result, {
+            dataClass: $.pivotUtilities.SubtotalPivotData,                    
+            rows: ["校區","校區電號別","收費年"],
+            cols: ["收費月"] ,
+            aggregator: $.pivotUtilities.aggregators["Integer Sum"](["用電數"]),
+            sorters: {
+                "校區": $.pivotUtilities.sortAs(["蘭潭","民雄","新民","林森"]),
+            },
+            renderer: $.pivotUtilities.subtotal_renderers["Table With Subtotal Row Heatmap"],                    
+            renderers:$.extend(
+                $.pivotUtilities.subtotal_renderers,
+                $.pivotUtilities.plotly_renderers,
+                $.pivotUtilities.d3_renderers,
+                $.pivotUtilities.export_renderers
+                ),
+            rendererOptions: {                        
+                rowSubtotalDisplay: {
+                disableAfter: 1
+                },                        
+                colSubtotalDisplay: {
+                    disableAfter: 0
+                },
             }
     });   
 }
